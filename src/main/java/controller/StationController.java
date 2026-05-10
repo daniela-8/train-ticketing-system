b56dc2be8d78be36641dcbc043709ee7c4fb2bd6
Daniela <bigdaniela9@gmail.com>
2026-05-10 14:06:18 +0300

    Implement REST controllers for stations and routes

--- src/main/java/controller/StationController.java
+++ src/main/java/controller/StationController.java
@@ -1,4 +1,41 @@
 package controller;
 
+import domain.Station;
+import network.dto.DtoMapper;
+import network.dto.StationDto;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import service.ITicketingService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/stations")
 public class StationController {
+
+    private static final Logger logger = LogManager.getLogger(StationController.class);
+    private final ITicketingService ticketingService;
+
+    @Autowired
+    public StationController(ITicketingService ticketingService) {
+        this.ticketingService = ticketingService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<StationDto>> getAllStations() {
+        logger.info("REST Request received: GET /api/stations");
+        List<Station> stations = ticketingService.getAllStations();
+
+        List<StationDto> dtos = stations.stream()
+                .map(DtoMapper::toDto)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(dtos);
+    }
 }
